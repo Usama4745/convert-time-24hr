8cd5deee60324621bab12203bf2fadffd41dafe4
Lau Sheng Cher <garylau@live.com>
2021-10-23 00:03:20 +0800

    Clean up redundancy calculation

--- index.js
+++ index.js
@@ -1,93 +1,66 @@
-module.exports = {
-    convertTimeTo24hrs: (hours, minutes, seconds, amorpm) => {
+function verifyInput(hours, minutes, seconds, to12hrs) {
+	if (typeof hours !== "number") {
+		throw new Error("hours should be integer value");
+	}
+	if (typeof minutes !== "number") {
+		throw new Error("minutes should be integer value");
+	}
+	if (typeof seconds !== "number") {
+		throw new Error("seconds should be integer value");
+	}
+	if (hours > 12 + 12 * to12hrs || hours < 1 - to12hrs) {
+		throw new Error("hours should be between or equal to 1 and 12");
+	}
+	if (minutes >= 60 || minutes < 0) {
+		throw new Error("minutes should be between 0 and 60");
+	}
+	if (seconds >= 60 || seconds < 0) {
+		throw new Error("seconds should be between 0 and 60");
+	}
+}
 
+module.exports = {
+
+    convertTimeTo24hrsAsString: (hours, minutes, seconds, amorpm) => {
 
         try {
-            if (typeof hours !== 'number') {
-                throw new Error("hours should be integer value");
-            }
-            if (typeof minutes !== 'number') {
-                throw new Error("minutes should be integer value");
-            }
-            if (typeof seconds !== 'number') {
-                throw new Error("seconds should be integer value");
-            }
-            if (typeof amorpm !== 'string') {
+            verifyInput(hours, minutes, seconds, false);
+            if (typeof amorpm !== "string") {
                 throw new Error("amorpm should be string");
-            }
-            if (hours > 12 || hours < 1) {
-                throw new Error("hours should be between or equal to 1 and 12");
-            }
-            if (minutes >= 60 || minutes < 0) {
-                throw new Error("minutes should be between 0 and 60");
-            }
-            if (seconds >= 60 || seconds < 0) {
-                throw new Error("seconds should be between 0 and 60");
             }
             if (amorpm.toUpperCase() !== "AM" && amorpm.toUpperCase() !== "PM") {
                 throw new Error("amorpm should be AM or PM");
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.error(e, e.stack);
         }
-
-
-        let timeIn24hrs = 0;
-        if (amorpm.toUpperCase() === 'PM' && hours === 12) {
-            timeIn24hrs = 12;
-        }
-        else if (amorpm.toUpperCase() === 'PM') {
-            timeIn24hrs = hours + 12;
-        }
-        else if (amorpm.toUpperCase() === 'AM' && hours === 12) {
-            timeIn24hrs = 00;
-        }
-        else {
-            timeIn24hrs = hours;
-        }
-        return ('0' + timeIn24hrs).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+    
+        return (
+            (
+                "0" +
+                (amorpm.toUpperCase() === "PM" ? (hours + 12) % 12 || hours : hours % 12)
+            ).slice(-2) +
+            ":" +
+            ("0" + minutes).slice(-2) +
+            ":" +
+            ("0" + seconds).slice(-2)
+        );
     },
-    convertTimeTo12hrs: (hours, minutes, seconds) => {
 
+    convertTimeTo12hrsAsString: (hours, minutes, seconds) => {
 
         try {
-            if (typeof hours !== 'number') {
-                throw new Error("hours should be integer value");
-            }
-            if (typeof minutes !== 'number') {
-                throw new Error("minutes should be integer value");
-            }
-            if (typeof seconds !== 'number') {
-                throw new Error("seconds should be integer value");
-            }
-            if (hours > 23 || hours < 0) {
-                throw new Error("hours should be between 0 and 24");
-            }
-            if (minutes >= 60 || minutes < 0) {
-                throw new Error("minutes should be between 0 and 60");
-            }
-            if (seconds >= 60 || seconds < 0) {
-                throw new Error("seconds should be between 0 and 60");
-            }
-        }
-        catch (e) {
+            verifyInput(hours, minutes, seconds, true);
+        } catch (e) {
             console.error(e, e.stack);
         }
-
-
-        if (hours == 12) {
-            return 12 + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2) + ' PM';
-        }
-        if (hours > 12) {
-            hours = hours - 12;
-            return hours + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2) + ' PM';
-        }
-        else if (hours == 0) {
-            return 12 + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2) + ' AM';
-        }
-        else {
-            return hours + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2) + ' AM';
-        }
-    }
+        return (
+            ("0" + (hours % 12 || 12)).slice(-2) +
+            ":" +
+            ("0" + minutes).slice(-2) +
+            ":" +
+            ("0" + seconds).slice(-2) +
+            (hours >= 12 ? " PM" : " AM")
+        );
+    },
 }
